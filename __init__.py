22186b02c2d5a5fafc0e4bc073a8265041ccc204
pferdwurst <mamedalin@gmail.com>
2014-03-18 17:39:42 -0400

    There are always new things to ignore

--- __init__.py
+++ __init__.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from flask import render_template, url_for, redirect
+
+
+from flask import make_response
+from functools import update_wrapper
 
 app = Flask(__name__)
+
+# app.config.from_object('parkyou.default_settings')
+# $ export PARKYOU_SETTINGS=conf/settings.cfg
+app.config.from_envvar('PARKYOU_SETTINGS')
+
+
 app.config["MONGODB_SETTINGS"] = {'DB': "parku"}
-app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
 
 db = MongoEngine(app)
 
+from parkyou.data import dbops
+import json
 
 def get_polylines():
      polylines = []
@@ -14,7 +27,34 @@ def get_polylines():
      for reg in regs:
          signs = dbops.find_signs_for_regulation(reg)
          polylines.append(signs)
+         
      return polylines
+
+
+def nocache(f):
+    def new_func(*args, **kwargs):
+        resp = make_response(f(*args, **kwargs))
+        resp.cache_control.no_cache = True
+        return resp
+    return update_wrapper(new_func, f)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Happy Parking!"
+
+
+@app.route('/map')
+@nocache
+def show_map():
+    latitude = 40.776366
+    longitude =  -73.943280
+    zoom = 14
+    regs = get_polylines()
+    
+    return render_template("nyc.html", API_KEY = app.config["GOOGLE_MAP_KEY"], LAT = latitude, LONG = longitude, ZOOM = zoom, js = url_for('static', filename='polyline.js'), regulations = regs )
+
 
 if __name__ == '__main__':
     app.run()
